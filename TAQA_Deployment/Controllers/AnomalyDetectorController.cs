0d327326bd9aded3035d607e27186422afeab67a
esemsc-av1824 <ashwin.vel24@imperial.ac.uk>
2025-08-28 13:07:06 +0100

    Add files via upload

---
+++ TAQA_Deployment/Controllers/AnomalyDetectorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace taqa.polaris.sit.Controllers
+{
+    [Route("AnomalyDetector")]
+    public class AnomalyDetectorController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index() => View();
+    }
+}
